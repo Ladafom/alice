a10c3060815107894e9ddea680c1381e8d013cbe
Lada Fomina <ladafmn@gmail.com>
2023-05-20 12:24:31 +0700

    start doing mailer

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
-    attr_accessor :remember_token
-    before_save { self.email = email.downcase }
+    attr_accessor :remember_token, :activation_token
+    before_save :downcase_email
+    before_create :create_activation_digest
     validates :name, presence: true, length: { maximum: 50 }
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :email, presence: true, length: { maximum: 255 },
@@ -8,7 +9,16 @@ class User < ActiveRecord::Base
                         uniqueness: { case_sensitive: false }
     has_secure_password
     validates :password, length: { minimum: 6 }, allow_blank: true
-
+    # Преобразует адрес эл почты в нижний регистр
+    def downcase_email
+        self.email = email.downcase
+    end
+    #
+    def create_activation_digest
+        self.activation_token = User.new_token 
+        self.activation_digest = User.digest(activation_token)
+    end
+    
     # Возвращает дайджест для указанной строки.
     def User.digest(string)
         cost = ActiveModel::SecurePassword.min_cost ? 
